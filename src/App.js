488f78c1a44353b382f038c42634f3647f4aacf8
Kuddleman <edl1155@gmail.com>
2022-01-02 18:01:40 -0800

    add data for major arcana

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import './App.css';
 import { Header } from './components/Header'
 import {CardHolder} from './components/CardHolder'
 import { Card } from './components/Card'
+import TAROT_CARDS from './cardsData';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello from App</h1>
+      <h1>{TAROT_CARDS[0].title}</h1>
+      <h2>{TAROT_CARDS[0].suit}</h2>
+      <img src={TAROT_CARDS[21].src} alt="tarot card" height="400px" width="230px" /> 
       <Header />
       <CardHolder />
       <Card />
---
+++ src/cardsData.js
@@ -0,0 +1,184 @@
+const TAROT_CARDS = 
+    [
+        {
+            id: 1,
+            title: "The Fool",
+            isMajorArcana: true,
+            isCourtCard: false,
+            suit: "Trumps",
+            src: 'images/rwfool.jpg'
+        },
+        {
+            id: 2,
+            title: 'The Magician',
+            isMajorArcana: true,
+            isCourtCard: false,
+            suit: 'Trumps',
+            src: 'images/rwmagician.jpg'  
+        },
+        {
+            id: 3,
+            title: 'The High Priestess',
+            isMajorArcana: true,
+            isCourtCard: false,
+            suit: 'Trumps',
+            src: 'images/rwhighpriestess.jpg'  
+        },
+        {
+            id: 4,
+            title: 'The Empress',
+            isMajorArcana: true,
+            isCourtCard: false,
+            suit: 'Trumps',
+            src: 'images/rwempress.jpg'  
+        },
+        {
+            id: 5,
+            title: 'The Emperor',
+            isMajorArcana: true,
+            isCourtCard: false,
+            suit: 'Trumps',
+            src: 'images/rwemperor.jpg'  
+        },
+        {
+            id: 6,
+            title: 'The Hierophant',
+            isMajorArcana: true,
+            isCourtCard: false,
+            suit: 'Trumps',
+            src: 'images/rwhierophant.jpg'  
+        },
+        {
+            id: 7,
+            title: 'The Lovers',
+            isMajorArcana: true,
+            isCourtCard: false,
+            suit: 'Trumps',
+            src: 'images/rwlovers.jpg'  
+        },
+        {
+            id: 8,
+            title: 'The Chariot',
+            isMajorArcana: true,
+            isCourtCard: false,
+            suit: 'Trumps',
+            src: 'images/rwchariot.jpg'  
+        },
+        {
+            id: 9,
+            title: 'Strength',
+            isMajorArcana: true,
+            isCourtCard: false,
+            suit: 'Trumps',
+            src: 'images/rwstrength.jpg'  
+        },
+        {
+            id: 10,
+            title: 'The Hermit',
+            isMajorArcana: true,
+            isCourtCard: false,
+            suit: 'Trumps',
+            src: 'images/rwhermit.jpg'  
+        },
+        {
+            id: 11,
+            title: 'The Wheel of Fortune',
+            isMajorArcana: true,
+            isCourtCard: false,
+            suit: 'Trumps',
+            src: 'images/rwwheelfortune.jpg'  
+        },
+        {
+            id: 12,
+            title: 'Justice',
+            isMajorArcana: true,
+            isCourtCard: false,
+            suit: 'Trumps',
+            src: 'images/rwjustice.jpg'  
+        },
+        {
+            id: 13,
+            title: 'The Hanged Man',
+            isMajorArcana: true,
+            isCourtCard: false,
+            suit: 'Trumps',
+            src: 'images/rwhangedman.jpg'  
+        },
+        {
+            id: 14,
+            title: 'Death',
+            isMajorArcana: true,
+            isCourtCard: false,
+            suit: 'Trumps',
+            src: 'images/rwdeath.jpg'  
+        },
+        {
+            id: 15,
+            title: 'Temperance',
+            isMajorArcana: true,
+            isCourtCard: false,
+            suit: 'Trumps',
+            src: 'images/rwtemperance.jpg'  
+        },
+        {
+            id: 16,
+            title: 'The Devil',
+            isMajorArcana: true,
+            isCourtCard: false,
+            suit: 'Trumps',
+            src: 'images/rwdevil.jpg'  
+        },
+        {
+            id: 17,
+            title: 'The Tower',
+            isMajorArcana: true,
+            isCourtCard: false,
+            suit: 'Trumps',
+            src: 'images/rwtower.jpg'  
+        },
+        {
+            id: 18,
+            title: 'Death',
+            isMajorArcana: true,
+            isCourtCard: false,
+            suit: 'Trumps',
+            src: 'images/rwstar.jpg'  
+        },
+        {
+            id: 19,
+            title: 'The Moon',
+            isMajorArcana: true,
+            isCourtCard: false,
+            suit: 'Trumps',
+            src: 'images/rwmoon.jpg'  
+        },
+        {
+            id: 20,
+            title: 'The Sun',
+            isMajorArcana: true,
+            isCourtCard: false,
+            suit: 'Trumps',
+            src: 'images/rwsun.jpg'  
+        },
+        {
+            id: 21,
+            title: 'Judgement',
+            isMajorArcana: true,
+            isCourtCard: false,
+            suit: 'Trumps',
+            src: 'images/rwjudgement.jpg'  
+        },
+        {
+            id: 22,
+            title: 'The World',
+            isMajorArcana: true,
+            isCourtCard: false,
+            suit: 'Trumps',
+            src: 'images/rwworld.jpg'  
+        },
+        
+    ]
+
+    export default TAROT_CARDS
+
+
